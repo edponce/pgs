f4150a41f35b0f3ce8fac98016ccb734a70d52ae
edponce <edponce2010@gmail.com>
2017-09-05 16:28:36 -0400

    upload all files to repo

---
+++ extra/yosh/exit.py
@@ -0,0 +1,5 @@
+from constants import *
+
+
+def exit(args):
+    return SHELL_STATUS_STOP
---
+++ extra/yosh/getenv.py
@@ -0,0 +1,8 @@
+import os
+from constants import *
+
+
+def getenv(args):
+    if len(args) > 0:
+        print(os.getenv(args[0]))
+    return SHELL_STATUS_RUN
